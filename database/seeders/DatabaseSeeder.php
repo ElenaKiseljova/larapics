2ce9bb464494f22eac11a4f18f9f81694d354f6a
Elena Kiseljova <elena.ki@webest.ru>
2023-08-09 19:08:33 +0300

    2. Connected Storage and replace placeholder images to real files from Storage folder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,7 @@ namespace Database\Seeders;
 
 use App\Models\Image;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Storage;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,13 +15,14 @@ class DatabaseSeeder extends Seeder
    */
   public function run(): void
   {
-    // \App\Models\User::factory(10)->create();
+    $images = Storage::allFiles('images');
 
-    // \App\Models\User::factory()->create([
-    //     'name' => 'Test User',
-    //     'email' => 'test@example.com',
-    // ]);
+    foreach ($images as $image) {
+      if (strpos($image, '.DS_Store')) continue;
 
-    Image::factory(6)->create();
+      Image::factory()->create([
+        'file' => $image,
+      ]);
+    }
   }
 }
