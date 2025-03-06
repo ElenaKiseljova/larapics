558c6785196403ba756a5c3bf41d4396a91ce6e7
Elena Kiseljova <e.a.kiseljova@gmail.com>
2025-03-06 11:49:14 +0200

    15. Component attributes (merge, prepends)

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,3 +1,3 @@
-<div class="alert alert-{{ $validType }}">
+<div {{ $attributes->merge(['class' => "alert alert-{$validType}", 'role' => $attributes->prepends('alert')]) }}>
   Waste no more time arguing what a good man should be, be one. - Marcus Aurelius
 </div>
