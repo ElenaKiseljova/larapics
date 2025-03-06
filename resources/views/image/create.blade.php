f1bfd986fb14a10abf3825d8a65108c528164b5c
Elena Kiseljova <e.a.kiseljova@gmail.com>
2025-03-06 14:34:35 +0200

    25. Refactoring the Upload image Form

--- resources/views/image/create.blade.php
+++ resources/views/image/create.blade.php
@@ -1,29 +1,51 @@
 <x-layout title="Upload new image">
-  <h1>Upload new Image</h1>
+  <div class="container-fluid mt-4">
+    <div class="row">
+      <div class="col-md-6 offset-md-3">
+        <div class="card">
+          <div class="card-header">Upload your photo</div>
 
-  <x-form action="{{ route('images.store') }}" method="post" enctype="multipart/form-data">
-    <div class="">
-      <label for="file">
-        File
-      </label>
+          <div class="card-body">
+            <x-form action="{{ route('images.store') }}" method="post" enctype="multipart/form-data">
+              <div class="mb-3">
+                <label class="form-label" for="file">Photo</label>
+                <input class="form-control @error('file') is-invalid @enderror" type="file" id="file"
+                  name="file">
+                @error('file')
+                  <div class="invalid-feedback">{{ $message }}</div>
+                @else
+                  <div class="form-text">
+                    You can only upload your photo in following types: jpg & png
+                  </div>
+                @enderror
+              </div>
 
-      <input type="file" name="file" id="file">
-      @error('file')
-        <div class="">{{ $message }}</div>
-      @enderror
+              <div class="mb-3">
+                <label class="form-label" for="title">Photo Title</label>
+                <input type="text" name="title" class="form-control @error('title') is-invalid @enderror"
+                  value="{{ old('title') }}">
+                @error('title')
+                  <div class="invalid-feedback">
+                    {{ $message }}
+                  </div>
+                @enderror
+              </div>
+              {{-- <div class="mb-3">
+                <label class="form-label" for="title">Photo Tags</label>
+                <input type="text" class="form-control">
+                <div class="form-text">
+                  Separate your tags with comma
+                </div>
+              </div> --}}
+              <div class="mb-3">
+                <button type="submit" class="btn btn-primary">Upload</button>
+                <a href="{{ route('images.index') }}" class="btn btn-outline-secondary">Cancel</a>
+              </div>
+            </x-form>
+          </div>
+        </div>
+
+      </div>
     </div>
-
-    <div class="">
-      <label for="title">
-        Title
-      </label>
-
-      <input type="text" name="title" id="title" value="{{ old('title') }}">
-      @error('title')
-        <div class="">{{ $message }}</div>
-      @enderror
-    </div>
-
-    <button type="submit">Upload</button>
-  </x-form>
+  </div>
 </x-layout>
