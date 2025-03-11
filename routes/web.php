2ac9346f2c35f32b4481bd9fcd3a1b99819bdbf2
Elena Kiseljova <e.a.kiseljova@gmail.com>
2025-03-11 18:34:03 +0200

    36. Authorizing images resource

--- routes/web.php
+++ routes/web.php
@@ -17,24 +17,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', ListImageController::class)->name('images.index');
-Route::get('/images/{image}', ShowImageController::class)->name('images.show');
-
-Route::get('/images/', [ImageController::class, 'create'])->name('images.create');
-Route::post('/images/', [ImageController::class, 'store'])->name('images.store');
-
-Route::get('/images/{image}/edit', [ImageController::class, 'edit'])->name('images.edit')
-    // 6
-    // ->middleware('can:update,image')
-
-    // 7
-    // ->can('update', 'image')
-;
-Route::put('/images/{image}', [ImageController::class, 'update'])->name('images.update');
-Route::delete('/images/{image}', [ImageController::class, 'destroy'])->name('images.destroy');
-
-Route::view('/test-blade', 'test');
-
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/', ListImageController::class)->name('images.all');
+Route::get('/images/{image}', ShowImageController::class)->name('images.show');
+Route::resource('/account/images', ImageController::class)->except('show');
+
+// Route::get('/images', [ImageController::class, 'index'])->name('images.index');
+// Route::get('/images/create', [ImageController::class, 'create'])->name('images.create');
+// Route::post('/images', [ImageController::class, 'store'])->name('images.store');
+// Route::get('/images/{image}/edit', [ImageController::class, 'edit'])->name('images.edit')
+//     // 6
+//     // ->middleware('can:update,image')
+
+//     // 7
+//     // ->can('update', 'image')
+// ;
+// Route::put('/images/{image}', [ImageController::class, 'update'])->name('images.update');
+// Route::delete('/images/{image}', [ImageController::class, 'destroy'])->name('images.destroy');
+
+// Route::view('/test-blade', 'test');
