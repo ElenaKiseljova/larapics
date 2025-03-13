124d21b4086561b76fff367cbe03088709ad702a
Elena Kiseljova <e.a.kiseljova@gmail.com>
2025-03-13 09:14:14 +0200

    fix: role column type

--- database/migrations/2025_03_11_141710_add_role_to_users.php
+++ database/migrations/2025_03_11_141710_add_role_to_users.php
@@ -7,26 +7,26 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::table('users', function (Blueprint $table) {
-            // $table->string('role')->default(Role::Author);
+  /**
+   * Run the migrations.
+   */
+  public function up(): void
+  {
+    Schema::table('users', function (Blueprint $table) {
+      // $table->string('role')->default(Role::Author);
 
-            // PHP 8 >= 8.1.0
-            $table->string('role')->default(Role::Author->value);
-        });
-    }
+      // PHP 8 >= 8.1.0
+      $table->enum('role', array_column(Role::cases(), 'value'))->default(Role::Author->value);
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('role');
-        });
-    }
+  /**
+   * Reverse the migrations.
+   */
+  public function down(): void
+  {
+    Schema::table('users', function (Blueprint $table) {
+      $table->dropColumn('role');
+    });
+  }
 };
