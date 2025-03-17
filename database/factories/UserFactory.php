8da06ddcfff6489973af7b16ac9c4625beaac943
Elena Kiseljova <e.a.kiseljova@gmail.com>
2025-03-17 10:08:50 +0200

    47. Modify the user profile schema

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -10,29 +10,35 @@ use Illuminate\Support\Str;
  */
 class UserFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition(): array
-    {
-        return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
-        ];
-    }
+  /**
+   * Define the model's default state.
+   *
+   * @return array<string, mixed>
+   */
+  public function definition(): array
+  {
+    return [
+      'name' => fake()->name(),
+      'email' => $email = fake()->unique()->safeEmail(),
+      'username' => strstr($email, '@', true) . mt_rand(100, 120),
+      'profile_image' => null,
+      'cover_image' => null,
+      'city' => mt_rand(0, 1) ? fake()->city() : null,
+      'country' => mt_rand(0, 1) ? fake()->country() : null,
+      'about_me' => mt_rand(0, 1) ? fake()->text() : null,
+      'email_verified_at' => now(),
+      'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+      'remember_token' => Str::random(10),
+    ];
+  }
 
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
-    public function unverified(): static
-    {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
-    }
+  /**
+   * Indicate that the model's email address should be unverified.
+   */
+  public function unverified(): static
+  {
+    return $this->state(fn(array $attributes) => [
+      'email_verified_at' => null,
+    ]);
+  }
 }
