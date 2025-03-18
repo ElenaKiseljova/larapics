1b0fc0e7d50dea42ca03e2dfcf767906f8f47746
Elena Kiseljova <e.a.kiseljova@gmail.com>
2025-03-18 21:13:07 +0200

    63. Managing comments - Replying to comments (part 2 of 2)

--- resources/views/comments/index.blade.php
+++ resources/views/comments/index.blade.php
@@ -21,22 +21,27 @@
         @foreach ($comments as $index => $comment)
           <tr class="{{ $updated != $comment->id ? '' : ($comment->approved ? 'table-success' : 'table-danger') }}">
             <td>{{ $comments->firstItem() + $index }}</td>
-            <td>{{ $comment->user->name }}</td>
+            <td>{{ '@' . $comment->user->username }}</td>
             <td>{{ $comment->body }}</td>
             <td>
               <a href="{{ $comment->image->permalink() }}">
                 <img src="{{ $comment->image->fileUrl() }}" width="100" />
-
               </a>
             </td>
             <td>
-              <x-form method="put" action="{{ route('comments.update', $comment->id) }}" style="display:inline;">
-                <input type="hidden" name="approved" value="{{ $comment->approved ? 0 : 1 }}">
-                <button class="btn btn-sm btn-outline-{{ $comment->approved ? 'warning' : 'success' }}">
-                  {{ $comment->approved ? 'Unapprove' : 'Approve' }}
-                </button>
-              </x-form>
-              <a href="#" class="btn btn-sm btn-outline-primary">Reply</a>
+              @if (auth()->id() === $comment->user_id)
+                <a href="#" class="btn btn-sm btn-outline-success disabled">Approve</a>
+                <a href="#" class="btn btn-sm btn-outline-primary disabled">Reply</a>
+              @else
+                <x-form method="put" action="{{ route('comments.update', $comment->id) }}" style="display:inline;">
+                  <input type="hidden" name="approved" value="{{ $comment->approved ? 0 : 1 }}">
+                  <button class="btn btn-sm btn-outline-{{ $comment->approved ? 'warning' : 'success' }}">
+                    {{ $comment->approved ? 'Unapprove' : 'Approve' }}
+                  </button>
+                </x-form>
+                <a href="{{ route('comments.reply.create', $comment->id) }}"
+                  class="btn btn-sm btn-outline-primary">Reply</a>
+              @endif
               <x-form method="delete" action="{{ route('comments.destroy', $comment->id) }}" style="display:inline;"
                 onsubmit="return confirm('Are you sure?')">
                 <button class="btn btn-sm btn-outline-danger">
